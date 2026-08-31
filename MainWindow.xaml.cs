7d8d85993d99f5708c4eb250b951cbe46d12c401
supamodd <supamodd@DESKTOP-AE5N24E>
2026-08-31 10:15:23 +0300

    tray fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using Application = System.Windows.Application;
 
 namespace SupaTweaker;
 
